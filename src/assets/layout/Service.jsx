7100d7feaf8686d0ddb49428d68787b3ea8f858e
rahuldevsutradhar <rahulkumar016332@gmail.com>
2025-04-13 21:12:40 +0600

    service part

--- src/assets/layout/Service.jsx
+++ src/assets/layout/Service.jsx
@@ -5,18 +5,40 @@ import HeadingOne from '../../components/HeadingOne'
 import SubHeading from '../../components/SubHeading'
 import Button from '../../components/Button'
 import ServiceList from '../../components/ServiceList'
+import LogoWeb from '../../assets/web.png'
+import software from '../../assets/software.png'
+import content from '../../assets/content.png'
+import merketing from '../../assets/merketing.png'
+import support from '../../assets/support.png'
 
 const Service = () => {
   return (
     <section>
         <Container>
-            <Flex>
-                <div>
+            <Flex className='gap-[30px] flex-wrap '>
+                <div className='pr-[20px]'>
                     <HeadingOne classname='pb-[20px]' text='What We Do'/>
                     <SubHeading className='w-[350px] pb-[56px]' text='We are now a team of strategists, engineers, designers, and marketers who both use and develop technology '/>
                     <Button text='Contact Us'/>
                 </div>
-                <ServiceList/>
+                <ServiceList image={LogoWeb}
+                 heading='web Design & Dev' 
+                 title='Social Media has changed the way we interact & do business while creating'/>
+                <ServiceList image={software}
+                 heading='Software Dev ' 
+                 title='Content Marketing is the other fold of online advertisement. If you are looking to build'/>
+                <ServiceList image={content}
+                 heading='Content Writing' 
+                 title='Social Media has changed the way we interact & do business while creating a new avenue.'/>
+                <ServiceList image={merketing}
+                 heading='Digital Marketing' 
+                 title='Social Media has changed the way we interact & do business while creating a new avenue.'/>
+                <ServiceList image={support}
+                 heading='Support & Training' 
+                 title='Content Marketing is the other fold of online advertisement. If you are looking to build'/>
+                
+                
+                
             </Flex>
         </Container>
     </section>
--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import Image from './Image'
-import LogoWeb from '../assets/web.png'
 
-const ServiceList = () => {
+
+const ServiceList = ({image,heading,title}) => {
   return (
-    <div>
-        <Image src={LogoWeb} alt=''/>
-        <h1>web Design & Dev </h1>
-        <p>Social Media has changed the way we interact & do business while creating</p>
-        
-
-
+    <div className=' group/one w-[370px] rounded-[24px] hover:bg-[#6B62C5] bg-white shadow-xl duration-300'>
+        <Image src={image} alt='' className='h-[64px] w-[64px] mx-auto mt-[48px] mb-[32px]'/>
+        <h1 className='font-open text-3xl	font-bold	 leading-9 text-center mb-[16px] group-hover/one:text-white text-black duration-300'> {heading} </h1>
+        <p className='group-hover/one:text-[#E6E5F3] text-black font-paprika text-lg	font-regular leading-9 text-center text-thirdly w-[257px] mx-auto mb-[16px] duration-300'>{title}</p>
+        <h6 className='group-hover/one:text-white text-black group/two font-open text-[24px]	font-bold	 leading-6 text-center mb-[57px] relative duration-300'> Learn More
+        <span className='absolute group-hover/two:w-[150px] w-[20px] h-[40px] group-hover/one:bg-[#ffffff4d] bg-[#1bbf004d] -top-[6px] right-[110px] group-hover/two:rounded-l-[30px] rounded-r-[30px] duration-300'></span></h6>
     </div>
   )
 }
